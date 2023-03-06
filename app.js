7893e0112bf709653601cd4b2bbe46666ff2f3fa
Mahmut Enes <46057801+NBWolfer@users.noreply.github.com>
2023-03-06 14:02:18 +0300

    Initializing repo

---
+++ app.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./Routes/tourRoutes');
+const userRouter = require('./Routes/userRoutes');
+
+const app = express();
+
+// 1) Middlewares
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  // console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) Routes
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
